bd7dcb0e2362eb30620c2b50d271fecd32136160
AndrewInfantino <andrew_infantino@hotmail.com>
2026-08-17 02:26:43 -0400

    August 17 Update - 2AM

--- site.js
+++ site.js
@@ -30,6 +30,7 @@ const DESTS = [
   {n:'Cappadocia',s:'Turkey',u:'cappadocia.html'},
   {n:'Dominican Republic',s:'Punta Cana, Samaná & more',u:'dominican-republic.html'},
   {n:'Dubrovnik',s:'Croatia',u:'dubrovnik.html'},
+  {n:'Florence',s:'Bologna, Pisa & Italy',u:'florence.html'},
   {n:'Israel & Palestine',s:'Jerusalem, Bethlehem & Tel Aviv',u:'israel-palestine.html'},
   {n:'Istanbul',s:'Turkey',u:'istanbul.html'},
   {n:'Japan',s:'Osaka, Kyoto & Tokyo',u:'japan.html'},
